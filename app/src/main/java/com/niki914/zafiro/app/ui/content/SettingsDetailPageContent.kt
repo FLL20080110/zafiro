06c7562b84b0d2ffa9800b708bb26c2728ec939a
FLL20080110 <xfghvxdfy@163.com>
2026-09-06 11:53:00 +0800

    security: route audit settings page

--- app/src/main/java/com/niki914/zafiro/app/ui/content/SettingsDetailPageContent.kt
+++ app/src/main/java/com/niki914/zafiro/app/ui/content/SettingsDetailPageContent.kt
@@ -109,6 +109,11 @@ fun SettingsDetailPageContent(
         return
     }
 
+    if (group == ZafiroSettingsGroup.SecurityAudit) {
+        SecurityAuditSettingsContent()
+        return
+    }
+
     TODOPageContent()
     return
 }
