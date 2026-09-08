d20045672615c161e0ba6c48ca3a5c745a05ea4b
FLL20080110 <xfghvxdfy@163.com>
2026-09-08 16:20:18 +0800

    Test model-only Clash proxy parser

---
+++ libs/okia/src/test/java/com/niki914/okia/transport/ModelProxyHttpEngineTest.kt
@@ -0,0 +1,44 @@
+package com.niki914.okia.transport
+
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertThrows
+import org.junit.Test
+
+class ModelProxyHttpEngineTest {
+    @Test
+    fun blankProxyKeepsDirectTransport() {
+        assertNull(modelProxyHttpEngine("   "))
+    }
+
+    @Test
+    fun clashHttpProxyIsAccepted() {
+        assertNotNull(modelProxyHttpEngine("http://127.0.0.1:7890"))
+    }
+
+    @Test
+    fun clashSocks5ProxyIsAccepted() {
+        assertNotNull(modelProxyHttpEngine("socks5://127.0.0.1:7891"))
+    }
+
+    @Test
+    fun missingPortIsRejected() {
+        assertThrows(IllegalArgumentException::class.java) {
+            modelProxyHttpEngine("http://127.0.0.1")
+        }
+    }
+
+    @Test
+    fun credentialsAreRejectedSoTheyCannotLeakThroughProxyUrls() {
+        assertThrows(IllegalArgumentException::class.java) {
+            modelProxyHttpEngine("http://user:secret@127.0.0.1:7890")
+        }
+    }
+
+    @Test
+    fun unsupportedSchemeIsRejected() {
+        assertThrows(IllegalArgumentException::class.java) {
+            modelProxyHttpEngine("https://127.0.0.1:7890")
+        }
+    }
+}
